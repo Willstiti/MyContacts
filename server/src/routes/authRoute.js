0abd2130dc59d366dd74c8853def50f12827cec5
Willstiti <william.deneys@gmail.com>
2025-09-17 01:39:22 +0200

    swagger register

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -44,6 +44,47 @@ const { handleLogin } = require("../controllers/AuthController");
  *                   type: string
  */
 router.route('/login').post(handleLogin);
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Inscription d'un utilisateur
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Utilisateur créé avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: string
+ *       500:
+ *         description: Erreur serveur
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 Error_message:
+ *                   type: string
+ */
 router.route('/register').post(handleNewUser);
 
 module.exports = router;
